9a945cca656a9616eb0e6d96c897e750956a1892
marlener87 <53823616+marlener87@users.noreply.github.com>
2020-07-17 16:16:54 +0200

    4 commit
    crud produit
    modif DashboardController.php

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Auteur;
+use App\Entity\Produit;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -16,6 +18,7 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         return parent::index();
+
     }
 
     public function configureDashboard(): Dashboard
@@ -27,6 +30,8 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linktoCrud('Auteur', 'fas fa-pen-alt', Auteur::class);
+        yield MenuItem::linktoCrud('Produit', 'fas fa-book', Produit::class);
         // yield MenuItem::linkToCrud('The Label', 'icon class', EntityClass::class);
     }
 }
