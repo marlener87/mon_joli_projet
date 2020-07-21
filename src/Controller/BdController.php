f5782540af6529b72a1fdab30e0e279014d53505
marlener87 <53823616+marlener87@users.noreply.github.com>
2020-07-21 09:47:56 +0200

    MàJ mise en place couverture
    liste des BD par auteurs
    mise en place des couvertures

--- src/Controller/BdController.php
+++ src/Controller/BdController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Auteur;
+use App\Entity\Produit;
 use App\Repository\AuteurRepository;
+use App\Repository\ProduitRepository;
 
 class BdController extends AbstractController
 {
@@ -34,11 +36,30 @@ class BdController extends AbstractController
     }
 
     /**
-     * @Route("/bd/livre/1", name="bd_show")
+     * @Route("/bd/livre/{id}", name="bd_show")
      */
-    public function show()
+    public function show($id, ProduitRepository $repo)
     {
-        return $this->render('bd/show.html.twig');
-    }
+        $produits = $repo->findBy(array("auteur" => $id));
+        // $couvertures = ['BD000001', 'BD000007', 'BD000013']
 
+        $couvertures = array();
+        $dir = "images/";
+        if ($dossier = opendir($dir)) {
+            while (($item = readdir($dossier)) !== false) {
+                if($item[0] == '.') { continue; }
+                /* if (!in_array(end(explode('.', $item)), array('jpg', 'jpeg', 'png', 'gif'))) { continue; } */
+                $pos_point = strpos ($item, '.');
+                $item = substr($item, 0, $pos_point);
+                $couvertures[] = strtoupper($item);
+            }
+            closedir($dossier);
+            /* rsort($couvertures); */
+        }
+
+        return $this->render('bd/show.html.twig', [
+            'produits' =>$produits,
+            'couvertures' =>$couvertures
+        ]);
+    }
 }
