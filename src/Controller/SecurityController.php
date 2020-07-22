6e7fcf2789176573765a9a192b0a49e632581219
marlener87 <53823616+marlener87@users.noreply.github.com>
2020-07-22 13:42:29 +0200

    MàJ bouton connexion/deconnexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -56,4 +56,11 @@ class SecurityController extends AbstractController
     public function login() {
         return $this->render('security/login.html.twig');
     }
+
+    /**
+     * @Route("/deconnexion", name="security_logout")
+     */
+    public function logout() {
+        
+    }
 }
