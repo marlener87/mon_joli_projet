4886c57fa818cb28944a8fbee5e1d2a92d3238d7
marlener87 <53823616+marlener87@users.noreply.github.com>
2020-07-27 14:47:28 +0200

    formulaire contact
    formulaire de contact p78 doc symfony5

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ContactType;
 
 class ContactController extends AbstractController
 {
@@ -12,8 +13,10 @@ class ContactController extends AbstractController
      */
     public function index()
     {
+        $form = $this->createForm(ContactType::class);
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'contact_form' => $form->createView(),
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,13 +5,17 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
+            ->add('name')
+            ->add('from', EmailType::class)
+            ->add('message', TextareaType::class)
         ;
     }
 
