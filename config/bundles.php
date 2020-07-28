63b90010af68ef7d08c9861fa87ea1fa5dd44091
marlener87 <53823616+marlener87@users.noreply.github.com>
2020-07-28 16:00:07 +0200

    envoi mail (ne fonctionne pas)
    - réinstallation de WAMP64 (car ne fonctionnait plus) + dernière version PHP
    - réinstallation BDD
    - formulaire contact avec envoi dans la boîte mail (ne fonctionne pas)

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,15 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index()
+    public function index(Request $request, MailerInterface $mailer)
     {
         $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        // $this->addFlash('info', 'Quelques infos utlies');
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
+
+            $email = (new Email())
+                ->from($contactFormData['from'])
+                ->to('test.programmation87@gmail.com')
+                ->subject('Time for Symfony Mailer!')
+                ->text($contactFormData['message']);
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a été envoyé !');
+
+            return $this->redirectToRoute('contact');
+        }
 
         return $this->render('contact/index.html.twig', [
             'contact_form' => $form->createView(),
